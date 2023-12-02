113cf0d9afc0f0d43c829a5ba98c2e33dd84a4a6
JohanUnal29 <111397882+JohanUnal29@users.noreply.github.com>
2023-12-02 13:25:38 -0500

    checkout

--- src/controllers/tickets.controller.js
+++ src/controllers/tickets.controller.js
@@ -157,7 +157,7 @@ class TicketController {
       for (const cartItem of ticket.cart) {
         const product = await productService.getProductById(cartItem._id);
         if (product && product.stock >= cartItem.cantidad) {
-          await ticketService.updateProducts(cartItem._id, cartItem.cantidad);
+          // await ticketService.updateProducts(cartItem._id, cartItem.cantidad);
           updatedCart.push(cartItem);
         } else {
           return res.status(400).send({
@@ -170,45 +170,7 @@ class TicketController {
 
       const ticketDTO = new TicketDTO(ticketCode, ticket, updatedCart);
       await ticketService.addTicket(ticketDTO);
-
-      // const items = updatedCart.map((item) => {
-      //   return {
-      //     id: item._id, // Debes asignar un ID único a cada producto
-      //     title: item.title,
-      //     currency_id: "COP", // Asegúrate de que la moneda sea la correcta
-      //     picture_url: item.thumbnails, // URL de la imagen del producto
-      //     description: `Código: ${item.code}`, // Agrega información adicional si es necesario
-      //     quantity: item.quantity,
-      //     unit_price: item.price,
-      //   };
-      // });
-
-      // const preference = {
-      //   items: items,
-      //   payer: {
-      //     name: ticket.name,
-      //     surname: ticket.purchaser,
-      //     email: ticket.email,
-      //     phone: {
-      //       area_code: "57", // Ajusta según tus datos
-      //       number: ticket.phone,
-      //     },
-      //   },
-      //   back_urls: {
-      //     success: "http://localhost:3000/",
-      //     failure: "http://www.failure.com",
-      //     pending: "http://www.pending.com",
-      //   },
-      //   auto_return: "approved",
-      //   // Resto de la configuración de MercadoPago
-      // };
-
-      // // Luego, puedes llamar a la API de MercadoPago para crear la preferencia
-      // client.preferences
-      //   .create(preference)
-      //   .then((response) => res.status(200).send(response))
-      //   .catch((error) => res.status(400).send({ error: error.message }));
-
+  
       return res.send({ status: "OK", message: "Ticket successfully added" });
     } catch (error) {
       return res.status(400).send({
