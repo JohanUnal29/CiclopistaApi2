e3adc4cf6c871e900295d6d18eba2ac3ba20e325
JohanUnal29 <johan.ardilah@gmail.com>
2024-01-26 18:45:09 -0500

    Image products

--- src/middlewares/product.js
+++ src/middlewares/product.js
@@ -1,6 +1,6 @@
 export const productValidator = (req, res, next) => {
-    const { title, description, code, price, status, stock, category, subCategory, thumbnails } = req.body;
-    if (!title || !description || !code || !price || !status || !stock || !category || !subCategory || !thumbnails) {
+    const { title, description, code, price, status, stock, category, subCategory, image } = req.body;
+    if (!title || !description || !code || !price || !status || !stock || !category || !subCategory || !image) {
         return res.status(400).json({ msg: 'Cuidado, hay campos vacíos' });
     } else {
         return next();
