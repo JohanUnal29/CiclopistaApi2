7350ecc5db79ab5225c0f7e544ba3260e8995600
JohanUnal29 <johan.ardilah@gmail.com>
2024-01-28 15:50:11 -0500

    Image products multer

--- src/routes/product.router.js
+++ src/routes/product.router.js
@@ -1,7 +1,20 @@
 import { Router } from "express";
 import { productController } from "../controllers/products.controller.js";
 import { productValidator } from "../middlewares/product.js";
-import {checkAdmin} from "../middlewares/auth.js";
+import { checkAdmin } from "../middlewares/auth.js";
+import multer from "multer"
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "../IMG/products");
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now();
+        cb(null, uniqueSuffix + file.originalname);
+    },
+});
+
+const upload = multer({ storage: storage });
 
 const router = Router();
 
@@ -9,7 +22,7 @@ router.get("/", productController.getPaginatedProducts);
 router.get("/all", productController.getProducts);
 router.get("/:category", productController.getProductsByCategory);
 router.get("/id/:pid", productController.getProductById);
-router.post("/addproduct/:uid", checkAdmin, productController.addProduct);//quitar el check admin para test
+router.post("/addproduct/:uid", checkAdmin, upload.single("image"), productController.addProduct);//quitar el check admin para test
 router.put("/:pid/:uid", checkAdmin, productController.updateProduct);
 router.delete("/:pid/:uid", checkAdmin, productController.deleteProduct);
 
