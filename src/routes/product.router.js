db41b3fa5c10a3b241997bd9a9e68ba543c8da71
JohanUnal29 <johan.ardilah@gmail.com>
2024-03-17 16:53:23 -0500

    img

--- src/routes/product.router.js
+++ src/routes/product.router.js
@@ -7,7 +7,7 @@ import multer from "multer"
 
 const storage = multer.diskStorage({
     destination:function(req,file,cb){
-        cb(null,`${__dirname}/../IMG/products`)
+        cb(null,`/../IMG/products`)
     },
     filename:function(req,file,cb){
         cb(null,`${Date.now()}-${file.originalname}`)
